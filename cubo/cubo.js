6a9819eb03b89a29d3e9927203a4352f9e79b4a1
paccco <pacocarrascoconde@gmail.com>
2023-03-16 18:03:56 +0100

    Cubo terminado

--- cubo/cubo.js
+++ cubo/cubo.js
@@ -9,40 +9,61 @@ class cubo extends THREE.Object3D {
     this.createGUI(gui,titleGui);
 
     this.material = new THREE.MeshPhongMaterial({color: 0xCF0000});
-    
-    var caja = new THREE.Mesh(new THREE.BoxGeometry(1,1,1),this.material);
 
-    this.add(caja);
+    this.caja=this.createCaja();
+
+    this.add(this.caja);
+  }
+
+  createCaja(){
+    var caja_ = new THREE.Object3D();
+    var cajaBase = new THREE.Mesh(new THREE.BoxGeometry(1,1,1),this.material);
+
+    caja_.position.set(0,2,0); 
+    caja_.scale.set(this.guiControls.dX,this.guiControls.dY,this.guiControls.dZ);
+    caja_.add(cajaBase);
+
+    return caja_;
   }
   
   createGUI (gui,titleGui) {
     this.guiControls = {
-      dimensionesX : 1,
-      dimensionesY : 1,
-      dimensionesZ : 1
+      dX : 2,
+      dY : 2,
+      dZ : 2
     }
 
     var folder = gui.addFolder (titleGui);
 
-    folder.add (this.guiControls, 'dimensionesX', 1, 5, 0.1)
+    folder.add (this.guiControls, 'dX', 0.1, 5, 0.1)
       .name ('Dimensiones X : ')
-      .onChange ( (value) => this.caja.geometry.scale());
+      .onChange ( (value) => this.setTamanioX(value) );
     
-      folder.add (this.guiControls, 'dimensionesY', 1, 5, 0.1)
+    folder.add (this.guiControls, 'dY', 0.1, 5, 0.1)
       .name ('Dimensiones Y : ')
-      .onChange ( (value) => this.setTamanio (value) );
+      .onChange ( (value) => this.setTamanioY(value) );
     
-      folder.add (this.guiControls, 'dimensionesZ', 1, 5, 0.1)
+    folder.add (this.guiControls, 'dZ', 0.1, 5, 0.1)
       .name ('Dimensiones Z : ')
-      .onChange ( (value) => this.setTamanio (value) );
+      .onChange ( (value) => this.setTamanioZ(value) );
   }
 
   setAngulo () {
-    this.caja.rotation.z += 0.01;
+    this.caja.rotation.z+=0.01;
   }
-  
+
+  setTamanioX(valor){
+    this.caja.scale.set(valor,this.guiControls.dY,this.guiControls.dZ);
+  }
+  setTamanioY(valor){
+    this.caja.scale.set(this.guiControls.dX,valor,this.guiControls.dZ);
+  }
+  setTamanioZ(valor){
+    this.caja.scale.set(this.guiControls.dX,this.guiControls.dY,valor);
+  }
+
   update () {
-    //this.setAngulo();
+    this.setAngulo();
   }
 }
 
