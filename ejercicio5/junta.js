c8136ceff2d6badb8c40d0db7ee22a06c1536c66
paccco <pacocarrascoconde@gmail.com>
2023-04-10 20:20:47 +0200

    ejer 5 arreglado

--- ejercicio5/junta.js
+++ ejercicio5/junta.js
@@ -10,34 +10,54 @@ class junta extends THREE.Object3D {
     this.createGUI(gui,titleGui);
     this.animacion=true;
 
-    var material = new THREE.MeshNormalMaterial();
+    /**var caja=new THREE.BoxGeometry(5,5,2);
+    var caja2=new THREE.BoxGeometry(4.5,4.5,2);
+    var cono=new THREE.CylinderGeometry(0.01,0.7,1,12);
 
+    caja.translate(2.5,-2.5,0);
+    caja2.translate(2.5,-2.5,0);
+
+    cono.translate(2,-0.5,0);
+    
+    var mesh1=new THREE.Mesh(caja,new THREE.MeshNormalMaterial());
+    var mesh2=new THREE.Mesh(caja2,new THREE.MeshNormalMaterial());
+    var mesh3=new THREE.Mesh(cono,new THREE.MeshNormalMaterial());
+
+    var csg = new CSG();
+
+    csg.union([mesh1]);
+    csg.subtract([mesh2]);
+    csg.subtract([mesh3]);
+
+    this.add(csg.toMesh()); */
+
+    var material = new THREE.MeshNormalMaterial();
     var junta1geom=new THREE.BoxGeometry(2,0.25,1);
+    var junta2geom=new THREE.BoxGeometry(2,0.25,1);
+    var boqueteGeometria1=new THREE.CylinderGeometry(0.01,0.25,0.5,16);
+    var boqueteGeometria2=new THREE.CylinderGeometry(0.01,0.25,0.5,16);
 
     junta1geom.translate(1.001,0,0.501);
-
-    var junta2geom=new THREE.BoxGeometry(2,0.25,1);
 
     junta2geom.rotateZ(Math.PI/2);
     junta2geom.translate(0.126,-1,.501);
 
-    var boqueteGeometria1=new THREE.CylinderGeometry(0.000000001,0.25,0.5,16);
-
     boqueteGeometria1.translate(1,0,0.5);
-
-    var boqueteGeometria2=new THREE.CylinderGeometry(0.000000001,0.25,0.5,16);
 
     boqueteGeometria2.rotateZ(Math.PI/2);
     boqueteGeometria2.translate(0.1,-1,0.5);
 
     var junta1=new THREE.Mesh(junta1geom,material);
     var junta2=new THREE.Mesh(junta2geom,material);
-
     var boquete1=new THREE.Mesh(boqueteGeometria1,material);
     var boquete2=new THREE.Mesh(boqueteGeometria2,material);
 
     var csg=new CSG();
     
+    csg.union([junta1]);
+    csg.union([junta2]);
+    csg.subtract([boquete1]);
+
     /*var material = new THREE. MeshNormalMaterial ( ) ;
 
     var cilExt = new THREE. CylinderGeometry ( 5 , 5 , 10 , 24 , 1 ) ;
